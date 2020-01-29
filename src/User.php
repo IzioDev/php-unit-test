eaead75110e53a4effd9978527bb6439671131d5
Quentin <quentin.somveille3874@gmail.com>
2020-01-29 15:49:10 +0100

    feat(User:identifier)

--- src/User.php
+++ src/User.php
@@ -5,11 +5,24 @@ final class User
     private $lastName;
     private $firstName;
     private $birthDate;
+    private $identifier;
 
     public function __construct($lastName, $firstName, $birthDate)
     {
         $this->lastName = $lastName;
         $this->firstName = $firstName;
         $this->birthDate = $birthDate;
+        $this->identifier = $this->setIdentifier();
+    }
+
+    public function setIdentifier()
+    {
+        $identifier = trim(strtoupper($this->firstName)) . trim(strtoupper($this->lastName)) . trim(date_format($this->birthDate, 'dmY'));
+        return $identifier;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->identifier;
     }
 }
